3df72dd1923212b4b1e0b675bc078861a9aada3c
Andrea Imparato <imparato.andrea@gmail.com>
2018-09-03 09:30:34 +0200

    Random commit

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :main ^:skip-aot a40kc.core
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.github.kyleburton/clj-xpath "1.4.3"]])
--- src/a40kc/core.clj
+++ src/a40kc/core.clj
@@ -1,6 +1,31 @@
 (ns a40kc.core)
 
-(defn foo
+(require '[clojure.xml :as xml])
+(require '[clojure.java.io :as io])
+(require '[clj-xpath.core :as xpath])
+
+(def xml-file (slurp "Mordians.ros"))
+
+;; (defn parse-xml
+;;   []
+;;   (->
+;;    (io/file xml-file)
+;;    (xml/parse)
+;;    (xml-seq)))
+
+;; (def xml-file-parsed (parse-xml))
+
+;; (defn get-attrs
+;;   [xml]
+;;   (get-in xml [:tag]))
+
+;; (defn parse-xpath []
+;;   (xpath/xml->doc (slurp "Mordians.ros")))
+
+(defn parse-xml []
+  (xpath/$x "//selection[@type='model']//characteristic" xml-file))
+
+(defn -main
   "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+  [& args]
+  (print (map #(println %) (parse-xml))))
