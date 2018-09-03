78374e70c9466005d42feacb5fe908c9d04af11e
Andrea Imparato <imparato.andrea@gmail.com>
2018-09-04 00:42:43 +0200

    Parsing done

--- src/a40kc/core.clj
+++ src/a40kc/core.clj
@@ -7,24 +7,13 @@
 
 (def xml-file (slurp "test.ros"))
 
-;; (defn parse-xml
-;;   []
-;;   (->
-;;    (io/file xml-file)
-;;    (xml/parse)
-;;    (xml-seq)))
+(def models (map dom/attributes (xpath/lookup-nodeset "//selection[@type='model']//profile[@profileTypeName='Unit']" xml-file)))
+(def weapons (map dom/attributes (xpath/lookup-nodeset "//selection[@type='model']//profile[@profileTypeName='Weapon']" xml-file)))
+(def weapons-stats (map dom/attributes (xpath/lookup-nodeset "//selection[@type='model']//profile[@profileTypeName='Weapon']//characteristic" xml-file)))
 
-;; (def xml-file-parsed (parse-xml))
-
-;; (defn get-attrs
-;;   [xml]
-;;   (get-in xml [:tag]))
-
-;; (defn parse-xpath []
-;;   (xpath/xml->doc (slurp "Mordians.ros")))
-
-(defn parse-xml []
-  (dom/name (xpath/lookup-node "//selection" xml-file)))
+(doseq [x models] (println (:name x)))
+(doseq [x weapons] (println (:name x)))
+(doseq [x weapons-stats] (println (:name x) (:value x)))
 
 (defn -main
   "I don't do a whole lot."
