bf052069dad8e60429f9ce5846fd064f1967dc50
Andrea Imparato <imparato.andrea@gmail.com>
2019-03-23 20:29:30 +0100

    Add squads and wounds

--- src/a40kc/core.clj
+++ src/a40kc/core.clj
@@ -1,17 +1,9 @@
 (ns a40kc.core
   (:gen-class))
+;; TODO parse by category: HQ, Troops, Elite, Heavy Support
+;; TODO handle characteristics "*" (vehicles/monsters)
 
 (use '[clj-xpath.core])
-
-(def xml-file1 (slurp "test.ros"))
-
-(def xml-file2 (slurp "test.ros"))
-
-(def data1
-  (xml->doc xml-file1))
-
-(def data2
-  (xml->doc xml-file2))
 
 (defn get-xml-values [node]
   (select-keys (:attrs node) [:name :value]))
@@ -36,33 +28,58 @@
        (get-xml-values item)
        :unit-characteristics
        (get-list-xml-values
-        ($x "./profiles/profile[@profileTypeName='Unit']//characteristic" item))
+       ($x ".//profile[@profileTypeName='Unit']//characteristic" item))
        :unit-weapons
        (parse-weapons item)
        })
-    ($x "//selection[@type='model']" data)))
+    ;; troops "number" of units
+    (concat
+     ($x "//selection[@type='model']" data)
+     ($x "//selection[@type='unit']" data))))
 
 (defn read-file [file]
   (parse-data (slurp file)))
 
-;;((6 - BS + 1) * 1/6) * 100
-(defn odds-shooting [BS]
-  (int (* 100 (* (+ (- 6 BS) 1) (/ 1 6.0)))))
-
+(read-file "roster.ros")
 
 (def data-file
-  (read-file "test.ros"))
+  (read-file "roster.ros"))
 
 (defn get-unit-characteristic [unit name]
   (:value (first (filter #(= (:name %) name) (:unit-characteristics unit)))))
 
+(defn clean-characteristic [value]
+  (if value
+    (read-string (clojure.string/replace value "+" ""))))
+
+;;((6 - BS + 1) * 1/6) * 100
+(defn odds-characteristic [BS]
+  (let [cleaned-bs (clean-characteristic BS)]
+    (println cleaned-bs)
+    (if (integer? cleaned-bs)
+      (int (* 100 (* (+ (- 6 cleaned-bs) 1) (/ 1 6.0)))))))
+
+
+(defn odds-wounding [S T]
+  (let [comparison (- S T)]
+    (- 4 comparison)))
+
+(odds-wounding 4 4)
+
+(odds-characteristic (odds-wounding 3 4))
+
+(clean-characteristic "*")
+
 (map
  (fn [unit]
-   (do
-     (pprint (:unit-name unit))
-     (pprint (get-unit-characteristic unit "BS"))
-     (pprint (odds-shooting (get-unit-characteristic unit "BS")))))
- data-file)
+   {
+    :name (:name (:unit-name unit))
+    :BS (get-unit-characteristic unit "BS")
+    :shooting (odds-characteristic (get-unit-characteristic unit "BS"))
+    })
+   data-file)
+
+;; (data-file)
 
 ;; ( -main
 ;;   "I don't do a whole lot."
